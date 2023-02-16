07101ee3a839833c552bb94bd9f6461f5ee7ae44
anjalikumari2425 <anjalikumari2425@gmail.com>
2023-02-16 23:25:40 +0530

    Setup for user controller

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const userController = require('./controllers/user.controller');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
+app.use('/users',userController);
+
 app.get('/',(req,res)=> {
   res.send("Hello");
 })
