c431c1cd99270e746daf72e2e1a6f20fc7a908a1
anjalikumari2425 <anjalikumari2425@gmail.com>
2023-02-16 23:13:55 +0530

    Initial setup for backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
+app.get('/',(req,res)=> {
+  res.send("Hello");
+})
+
+app.listen(PORT, function(err){
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
