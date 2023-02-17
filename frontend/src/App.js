477f2553b9409cfd60b5606f4c32607b07f81db2
anjalikumari2425 <anjalikumari2425@gmail.com>
2023-02-17 18:39:21 +0530

    Connected add user details and show user details pages with button click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState} from "react"
+import UserForm from './components/UserForm';
+import UserDetails from './components/UserDetails';
 
 function App() {
+  const [show, setShow] = useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setShow(!show)}>{show? "Show User Details":"Add User"}</button>
+      {show? <UserForm/>:<UserDetails/>}
     </div>
   );
 }
